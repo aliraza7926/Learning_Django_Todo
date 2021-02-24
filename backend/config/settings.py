56c9c9f2da3f552255eaf2dc62b150615a14f9a3
Alireza Sadeghi <aliraza7926@gmail.com>
2021-02-24 08:23:35 +0330

    add restframework to installed app and configur it

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # 3rd party
+    'rest_framework',
+
     # local
     'todos',
 ]
@@ -121,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':[
+        'rest_framework.permission.AllowAny',
+    ]
+}
